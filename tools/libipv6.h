4898d508f28568ce88412f3607dd9f17b3bf1775
Fernando Gont <fgont@si6networks.com>
2013-11-07 02:12:01 -0800

    Fix Linux definitions

--- tools/libipv6.h
+++ tools/libipv6.h
@@ -239,6 +239,10 @@ struct ether_header
 
 typedef	u_int32_t tcp_seq;
 
+
+#ifdef __linux__
+/* Linux definition */
+
 /*
  * TCP header.
  * Per RFC 793, September, 1981.
@@ -267,7 +271,39 @@ struct tcp_hdr{
     u_int16_t th_sum;		/* checksum */
     u_int16_t th_urp;		/* urgent pointer */
 } __attribute__ ((__packed__));
-
+#else
+/* BSD definition */
+/*
+ * TCP header.
+ * Per RFC 793, September, 1981.
+ */
+struct tcphdr {
+	u_int16_t th_sport;		/* source port */
+	u_int16_t th_dport;		/* destination port */
+	tcp_seq	  th_seq;		/* sequence number */
+	tcp_seq	  th_ack;		/* acknowledgement number */
+#if _BYTE_ORDER == _LITTLE_ENDIAN
+	u_int32_t th_x2:4,		/* (unused) */
+		  th_off:4;		/* data offset */
+#endif
+#if _BYTE_ORDER == _BIG_ENDIAN
+	u_int32_t th_off:4,		/* data offset */
+		  th_x2:4;		/* (unused) */
+#endif
+	u_int8_t  th_flags;
+#define	TH_FIN	  0x01
+#define	TH_SYN	  0x02
+#define	TH_RST	  0x04
+#define	TH_PUSH	  0x08
+#define	TH_ACK	  0x10
+#define	TH_URG	  0x20
+#define	TH_ECE	  0x40
+#define	TH_CWR	  0x80
+	u_int16_t th_win;			/* window */
+	u_int16_t th_sum;			/* checksum */
+	u_int16_t th_urp;			/* urgent pointer */
+};
+#endif
 
 struct udp_hdr{
   u_int16_t uh_sport;		/* source port */
@@ -369,6 +405,7 @@ struct iface_list{
 
 
 #if !(defined (__FreeBSD__) || defined(__NetBSD__) || defined (__OpenBSD__) || defined(__APPLE__))
+/* Definitions for Linux */
 
 	#define ICMP6_NI_QUERY			139	/* node information request */
 	#define ICMP6_NI_REPLY			140	/* node information reply */
@@ -409,15 +446,15 @@ struct iface_list{
 	#define NI_QTYPE_NODEADDR	3 /* Node Addresses */
 	#define NI_QTYPE_IPV4ADDR	4 /* IPv4 Addresses */
 
-	#if _BYTE_ORDER == _BIG_ENDIAN
+	#if __BYTE_ORDER == __BIG_ENDIAN
 		#define NI_SUPTYPE_FLAG_COMPRESS	0x1
 		#define NI_FQDN_FLAG_VALIDTTL		0x1
-	#elif _BYTE_ORDER == _LITTLE_ENDIAN
+	#elif __BYTE_ORDER == __LITTLE_ENDIAN
 		#define NI_SUPTYPE_FLAG_COMPRESS	0x0100
 		#define NI_FQDN_FLAG_VALIDTTL		0x0100
 	#endif
 
-	#if _BYTE_ORDER == _BIG_ENDIAN
+	#if __BYTE_ORDER == __BIG_ENDIAN
 		#define NI_NODEADDR_FLAG_TRUNCATE	0x1
 		#define NI_NODEADDR_FLAG_ALL		0x2
 		#define NI_NODEADDR_FLAG_COMPAT		0x4
@@ -425,7 +462,7 @@ struct iface_list{
 		#define NI_NODEADDR_FLAG_SITELOCAL	0x10
 		#define NI_NODEADDR_FLAG_GLOBAL		0x20
 		#define NI_NODEADDR_FLAG_ANYCAST	0x40 /* just experimental. not in spec */
-	#elif _BYTE_ORDER == _LITTLE_ENDIAN
+	#elif __BYTE_ORDER == __LITTLE_ENDIAN
 		#define NI_NODEADDR_FLAG_TRUNCATE	0x0100
 		#define NI_NODEADDR_FLAG_ALL		0x0200
 		#define NI_NODEADDR_FLAG_COMPAT		0x0400
