893324d3d2c99de2fd3567b1d7f03def8d777a60
Fernando Gont <fgont@si6networks.com>
2013-11-07 16:18:23 -0800

    Fix Mac OS breakage

--- tools/libipv6.h
+++ tools/libipv6.h
@@ -271,8 +271,40 @@ struct tcp_hdr{
     u_int16_t th_sum;		/* checksum */
     u_int16_t th_urp;		/* urgent pointer */
 } __attribute__ ((__packed__));
+#elif defined(__APPLE__)
+/* Mac OS definition */
+
+/*
+ * TCP header.
+ * Per RFC 793, September, 1981.
+ */
+struct tcp_hdr{
+    u_int16_t th_sport;		/* source port */
+    u_int16_t th_dport;		/* destination port */
+    tcp_seq th_seq;		/* sequence number */
+    tcp_seq th_ack;		/* acknowledgement number */
+#  if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+    u_int8_t th_x2:4;		/* (unused) */
+    u_int8_t th_off:4;		/* data offset */
+#  endif
+#  if __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+    u_int8_t th_off:4;		/* data offset */
+    u_int8_t th_x2:4;		/* (unused) */
+#  endif
+    u_int8_t th_flags;
+#  define TH_FIN	0x01
+#  define TH_SYN	0x02
+#  define TH_RST	0x04
+#  define TH_PUSH	0x08
+#  define TH_ACK	0x10
+#  define TH_URG	0x20
+    u_int16_t th_win;		/* window */
+    u_int16_t th_sum;		/* checksum */
+    u_int16_t th_urp;		/* urgent pointer */
+} __attribute__ ((__packed__));
 #else
 /* BSD definition */
+
 /*
  * TCP header.
  * Per RFC 793, September, 1981.
