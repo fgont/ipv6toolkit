b145c691568b46054b9de469a57c3db5c7e68a07
Fernando Gont <fgont@si6networks.com>
2014-08-11 21:17:50 -0400

    Add defnition of struct ip6_ext for Solaris

--- tools/libipv6.h
+++ tools/libipv6.h
@@ -272,8 +272,8 @@ struct ip6_eh{
 /* Solaris does not define this one */
 #if defined(sun) || defined(__sun)
 	struct  ip6_ext {
-		u_int8_t ip6e_nxt;
-		u_int8_t ip6e_len;
+		uint8_t ip6e_nxt;
+		uint8_t ip6e_len;
 	} __attribute__ ((__packed__));
 #endif
 
