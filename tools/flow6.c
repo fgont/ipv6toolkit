e84bbde28889a09b14e29ca4770178ff74138b2e
Fernando Gont <fgont@si6networks.com>
2014-08-14 15:35:27 -0400

    Fix validation of key/signature

--- tools/flow6.c
+++ tools/flow6.c
@@ -443,7 +443,8 @@ int main(int argc, char **argv){
 						 * only if they are destined to those addresses
 						 */
 						idata.srcaddr.s6_addr32[2]= htonl((ntohl(idata.srcaddr.s6_addr32[2]) & 0xffff0000) | addr_sig);
-						idata.srcaddr.s6_addr32[3] = idata.srcaddr.s6_addr32[3] | htonl((uint16_t)(ntohl(idata.srcaddr.s6_addr32[3])>>16) ^ addr_key);
+						idata.srcaddr.s6_addr32[3]= htonl((ntohl(idata.srcaddr.s6_addr32[3]) & 0xffff0000) | \
+						                            ((uint16_t)(ntohl(idata.srcaddr.s6_addr32[3])>>16) ^ addr_key));
 
 						if(send_neighbor_solicit(&idata, &(idata.dstaddr)) == -1){
 							puts("Error while sending Neighbor Solicitation");
@@ -521,10 +522,11 @@ int main(int argc, char **argv){
 								}
 							}
 						}
-						else{
+						else if(idata.type == DLT_EN10MB && !(idata.flags & IFACE_LOOPBACK)){
 							if( (ntohl(pkt_ns->nd_ns_target.s6_addr32[2]) & 0x0000ffff) != addr_sig || \
-								(ntohl(pkt_ns->nd_ns_target.s6_addr32[3]) & 0x0000ffff) != ( (ntohl(pkt_ns->nd_ns_target.s6_addr32[3])>>16) ^ addr_key))
+								(ntohl(pkt_ns->nd_ns_target.s6_addr32[3]) & 0x0000ffff) != ( (ntohl(pkt_ns->nd_ns_target.s6_addr32[3])>>16) ^ addr_key)){
 								continue;
+							}
 
 							if(send_neighbor_advert(&idata, idata.pfd, pktdata) == -1){
 								puts("Error sending Neighbor Advertisement");
