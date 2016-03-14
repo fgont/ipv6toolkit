0a7f16c5e540ad35445d64af43a67be1a49e1189
Fernando Gont <fgont@si6networks.com>
2016-03-14 08:00:04 -0300

    Update toolkit version

--- tools/ipv6toolkit.h
+++ tools/ipv6toolkit.h
@@ -1,4 +1,4 @@
-#define SI6_TOOLKIT "SI6 Networks' IPv6 Toolkit v2.0 (Guille)"
+#define SI6_TOOLKIT "SI6 Networks' IPv6 Toolkit v2.1 (Jan)"
 #define	MAX_CMDLINE_OPT_LEN	40
 #define DATE_STR_LEN		40
 
